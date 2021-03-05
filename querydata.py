0991858e9475b1064041be868e5b7cec5c478c42
3jcn <31360292+3jcn@users.noreply.github.com>
2021-03-04 17:08:29 -0800

    Update querydata.py

--- querydata.py
+++ querydata.py
@@ -6,7 +6,7 @@ import webbrowser
 
 
 def talk(text):
-    engine = pyttsx3.init() #('sapi5')
+    engine = pyttsx3.init('dummy') #('sapi5')
     voices = engine.getProperty('voices')
     engine.setProperty('voice',voices[0].id)
     engine.setProperty('rate', 170)
