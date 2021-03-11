29f55c5ab1eef70dbf30bd340bf9f78a3a025851
3jcn <31360292+3jcn@users.noreply.github.com>
2021-03-10 16:58:09 -0800

    Update querydata.py

--- querydata.py
+++ querydata.py
@@ -8,12 +8,12 @@ import webbrowser
 # in 'driver.py' change "except Exception, e:" to "except Exception as e:"
 
 def talk(text):
-    engine = pyttsx3.init()
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice',voices[0].id)
-    engine.setProperty('rate', 170)
-    engine.say(text)
-    engine.runAndWait()
+    engineio = pyttsx3.init()
+    voices = engineio.getProperty('voices')
+    engineio.setProperty('voice',voices[0].id)
+    engineio.setProperty('rate', 170)
+    engineio.say(text)
+    engineio.runAndWait()
 
 def run_query(input):
     outp = ''
